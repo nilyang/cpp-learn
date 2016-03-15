e1a9d14250138f3bebf3458f578cb3fbc965622c
nilyang <nil.yang@qq.com>
2016-03-15 02:33:42 +0000

    1.add:完善Rectangle类及测试代码

---
+++ Rectangle/Readme.md
@@ -0,0 +1,26 @@
+### Rectangle 类实现
+
+打印了构造及析构过程
+###
+```C++
+{
+  //默认构造
+	Rectangle r1;
+	Rectangle r2(r1);
+	//构造，拷贝构造
+	std::cout << "r1 : "<<r1 << "\nr2: " << r2;
+
+	//构造赋值
+	Rectangle r3(400, 300, 500, 300);
+	r1 = r3;
+	std::cout << "r1:" << r1 << "\nr3: " << r3;
+
+	//堆内存分配，拷贝构造
+	Rectangle *pr4 = new Rectangle(r3);
+	std::cout << "pr4: " << pr4 << "\nr4: " << *pr4;
+
+	delete pr4;
+	std::cout << "destructed pr4:" << pr4 << "\n";
+}
+```
+附带执行结果图片截图
--- Rectangle/Rectangle.h
+++ Rectangle/Rectangle.h
@@ -2,14 +2,25 @@
 #ifndef __NILYANG_RECTANGLE
 #define __NILYANG_RECTANGLE
 
+#include<iostream>
+
+//前置声明
+class Shape;
+class Point;
+class Rectangle;
+
+//输出<<重载
+inline std::ostream& operator << (std::ostream& os,const Rectangle& rct);
+
+
 class Shape
 {
 public:
 	Shape(int num=0):no(num)
 	{
-		std::cout << "Shape constructor\n";
+		std::cout << "Shape default constructor\n";
 	}
-	//虚析构，子类
+	//虚析构
 	virtual ~Shape()
 	{
 		std::cout << "Shape constructor\n";
@@ -24,14 +35,20 @@ class Point
 public:
 	Point(int _x, int _y)
 		:x(_x),y(_y)
-	{}
+	{	
+		std::cout << "Point default constructor\n";
+	}
+	~Point() 
+	{
+		std::cout << "Point destructor\n";
+	}
+	//Point(const Point& other);
 	int getX() const { return x; }
 	int getY() const { return y; }
 private:
 	int x;
 	int y;
 };
-
 
 class Rectangle : public Shape
 {
@@ -40,32 +57,60 @@ private:
 	int height;//高
 	Point* leftUp;//左上角顶点位置
 public:
-	Rectangle(int _width, int _height, int x, int y)
-		:Shape(),width(_width),height(_height),leftUp{new Point(x,y)}
-	{}
+	Rectangle(int _width=0, int _height=0, int x=0, int y=0)
+		:Shape(),width(_width>0?_width:0)
+		,height(_height>0?_height:0)
+		,leftUp{new Point(x>0?x:0,y>0?y:0)}
+	{
+		std::cout << "Rectangle default constructor\n";
+	}
 	Rectangle(const Rectangle& other);//拷贝构造
 	Rectangle& operator=(const Rectangle& other);//构造赋值
-	~Rectangle();//析构
 
-	
+	int getWidth() const { return width; }
+	int getHeight() const { return height; }
+	Point& getPoint() const { return *leftUp; }
+
+	~Rectangle();//析构
 };
 
-//建议，如果两边指针都指着对象的话，直接赋值就可以，不需先删，提高性能
-
-inline Rectangle::Rectangle(const Rectangle & other)
+//拷贝构造，会先调用基类的构造函数，由内而外
+inline 
+Rectangle::Rectangle(const Rectangle & other):Shape()
 {
-	//TODO
+	std::cout << "Rectangle copy constructor\n"; 
+	if (this != &other) {
+		height = other.height;
+		width = other.width;
+		leftUp = new Point(*other.leftUp);//默认拷贝构造
+	}
 }
 
-inline Rectangle & Rectangle::operator=(const Rectangle & other)
+//拷贝构造
+inline Rectangle & 
+Rectangle::operator=(const Rectangle & other)
 {
-	// TODO: 在此处插入 return 语句
+	std::cout << "Rectangle asignment constructor\n";
+	
+	if (this == &other) {
+		return *this;
+	}
+
+	this->height = other.height;
+	this->width = other.width;
+	this->leftUp = new Point(*other.leftUp);
+
 	return *this;
 }
 
-inline Rectangle::~Rectangle()
+//析构函数，会先调用Rectangle本身的析构，再调用基类的析构~Shape()
+inline 
+Rectangle::~Rectangle()
 {
+	std::cout << "Rectangle destructor\n";
+	
 	delete leftUp;
 }
+
 
 #endif // !__NILYANG_RECTANGLE

9292aa14debdf3cc81611b9c9538e13d320a0786
nilyang <nil.yang@qq.com>
2016-03-24 05:34:41 +0000

    1.add: 添加新增的动态数组，存放剩下的面积大于50的子类 。TODO，但还是存在内存泄漏，暂时无法解决多态的问题

--- Circle/Circle.cpp
+++ Circle/Circle.cpp
@@ -3,48 +3,94 @@
 #include <random>
 #include <ctime>
 
+
 void Test_Circle()
 {
-	srand((unsigned)time(nullptr));
+    srand((unsigned)time(nullptr));
 
-	const int n = 20;
+    const int n = 20;
 
-  int half = int(n / 2);
+    int half = int(n / 2);
 
-	Shape *pShape[n];
+    Shape *pShape[n];
 
-	for (int i = 0; i < n; i++)
-	{
-		int x = rand() % 10;
-		int y = rand() % 10;
-		if (i < half ) {
-			int width = rand() % 9 + 1;
-			int height = rand() % 9 + 1;
-			pShape[i] = new Rectangle(i, width, height, x, y);
- 		}
-		else {
-			int r = rand() % 9 + 1;
-			pShape[i] = new Circle(i, r, x, y);
-		}
-		std::cout << *pShape[i] ;
-	}
+    for (int i = 0; i < n; i++)
+    {
+        int x = rand() % 10;
+        int y = rand() % 10;
+        if (i < half) {
+            int width = rand() % 9 + 1;
+            int height = rand() % 9 + 1;
+            pShape[i] = new Rectangle(i, width, height, x, y);
+        }
+        else {
+            int r = rand() % 9 + 1;
+            pShape[i] = new Circle(i, r, x, y);
+        }
+        pShape[i]->print();
+    }
 
-  //delete element which area less than 50
-	Shape *newShape[];
-	for (int i = 0; i < n; i++ )
-	{
-		if (pShape[i] != nullptr) {
-			delete pShape[i];
-		}
-	}
+    //delete element which area less than 50
+   
+    int m = n;
 
+    for (int i = 0, j = 0; i < n; i++)
+    {
+        if (!pShape[i]) {
+            continue;
+        }
+
+        if (pShape[i]->getArea() < 50) {
+            delete pShape[i];
+            pShape[i] = nullptr;
+            --m;
+        }
+    }
+     
+    std::cout << "n=" << n << ", m=" << m << "\n";
+
+    Shape **newShape = new Shape*[m];
+
+    for (int i = 0, j = 0; i < n; i++, j++) {
+        if (pShape[i] && j < m) {
+            newShape[j] = pShape[i]->clone();
+        }
+    }
+
+    //for (int i = 0; i < m; i++) {
+    //    if (newShape && newShape[i]) {
+    //        newShape[i]->print();//无法通过编译……，不知为什么，求解答
+    //    }
+    //}
+
+    for (int i = 0; i < n; i++)
+    {
+        if (pShape[i] != nullptr) {
+            delete pShape[i];
+            pShape[i] = nullptr;
+        }
+    }
+
+    //for (int i = 0; i < m; i++)
+    //{
+    //    if (newShape[i] != nullptr) {
+    //        delete newShape[i];
+    //    }
+    //}
+
+
+    delete[] newShape;
+    newShape = nullptr;
 }
 
 int main(int argc, char** argv) {
 
-	Test_Circle();
+    Test_Circle();
+    int m = 30;
+    //Shape **a = new Shape*[m];
+    //Shape **a = {};
 
-	system("pause");
+    system("pause");
 
-	return 0;
+    return 0;
 }
--- Circle/Circle.h
+++ Circle/Circle.h
@@ -4,13 +4,14 @@
 
 #include <iostream>
 
-//pre declare
+//前置声明
 class Shape;
 class Point;
 class Rectangle;
 class Circle;
+struct ShapeStack;
 
-//operator overload
+//输出<<重载
 //inline std::ostream& operator << (std::ostream& os, const Point& p);
 //inline std::ostream& operator << (std::ostream& os, const Rectangle& rct);
 //inline std::ostream& operator << (std::ostream& os, const Circle& c);
@@ -39,8 +40,12 @@ class Circle;
 //        << "\n";
 //}
 
+//多态
+//inline std::ostream& operator << (std::ostream& os, const Shape& base);
 
-inline std::ostream& operator << (std::ostream& os, const Shape& base);
+
+//友元测试多态
+inline void __print_shape(const Shape * ths);
 
 class Shape
 {
@@ -48,12 +53,21 @@ class Shape
     int type;
 public:
     Shape(int _num = 0, const int _type = 0)
-        :no(_num),type(_type)
+        :no(_num),type(_type) 
     {}
     int getNum() const { return no; }
     int getType() const { return type; }
     virtual int getArea() const = 0;
+    virtual Shape *clone() const = 0;
     virtual ~Shape() {}
+
+    void print() const
+    {
+        __print_shape(this);
+    }
+    //多态
+    friend void __print_shape(const Shape *ths);
+
 };
 
 class Point
@@ -65,7 +79,7 @@ public:
         :x(_x), y(_y)
     {}
     ~Point(){}
-
+    
     int getX() const { return x; }
     int getY() const { return y; }
 };
@@ -76,14 +90,36 @@ class Rectangle : public Shape
     int height;
     Point leftUp;
 public:
+    ~Rectangle() {}
     Rectangle(int no = 0, int w = 0, int h = 0, int x = 0, int y = 0)
         :Shape(no,0), width(w), height(h),leftUp(x,y)
     {}
-    ~Rectangle() {}
-
+    Rectangle(const Rectangle& other):Shape(other)
+    {
+        if (this != &other) {
+            this->width = other.width;
+            this->height = other.height;
+            this->leftUp = other.leftUp;
+        }
+    }
+    Rectangle& operator=(const Rectangle& other)
+    {
+        if (this == &other) {
+            return *this;
+        }
+        Shape::operator=(other);
+        this->width = other.width;
+        this->height = other.height;
+        this->leftUp = other.leftUp;
+    }
     virtual int getArea() const
     {
         return width * height;
+    }
+
+    virtual Rectangle* clone() const 
+    {
+        return new Rectangle(*this);
     }
 };
 class Circle : public Shape
@@ -105,13 +141,24 @@ public:
     {
         return (int)getAreaCircle();
     }
+
+    virtual Circle* clone() const
+    {
+        return new Circle(*this);
+    }
+
 };
 
 
-inline std::ostream & operator<<(std::ostream & os, const Shape & base)
+inline void __print_shape(const Shape *ths)
 {
     const char * s[2] = { "Rectangle", "Circle" };
 
-    return std::cout << s[base.getType()] << " : NO=" << base.getNum() << ", Area=" << base.getArea() << "\n";
+    std::cout << s[ths->getType()]
+              << " : NO=" << ths->getNum()
+              << ", Area=" << ths->getArea() << "\n";
 }
+
+
+
 #endif

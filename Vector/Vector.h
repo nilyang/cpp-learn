be28559f15e7ebf07a0ceabb09ed08ae93cb3539
nilyang <nil.yang@qq.com>
2016-04-08 09:18:46 +0800

    1.add:vector

---
+++ Vector/Vector.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef __NILYANG_VECTOR__
+
+
+#endif // !__NILYANG_VECTOR__
---
+++ Vector/Vector_test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"Vector.h"
+
+void Vector_Test()
+{
+
+}
+int main()
+{
+    Vector_Test();
+    system("pause");
+    return 0;
+}

59ef04826e57a9318a382aaeb237dbd42d4fd3fe
nilyang <nil.yang@qq.com>
2016-04-10 09:47:01 +0800

    1.add:完善代码

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,6 +1,23 @@
 #pragma once
 #ifndef __NILYANG_VECTOR__
+#include<vector>
 
-//TODO
+//
+template<typename T> 
+std::vector<T> my_not_equal_to(std::vector<T>& v1,  T equal_to);
+
+template<typename T>
+inline std::vector<T> my_not_equal_to(std::vector<T>& v1, T equal_to)
+{
+    std::vector<T> v2;
+    for (std::vector<T>::iterator it = v1.begin(); it < v1.end();it++)
+    {
+        if (*it != 0) {
+            v2.push_back(*it);
+        }
+    }
+
+    return v2;
+}
 
 #endif // !__NILYANG_VECTOR__
--- Vector/Vector_test.cpp
+++ Vector/Vector_test.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
+#include<string>
+#include<vector>
 #include"Vector.h"
+
+using std::string;
+using std::vector;
 
 void Vector_Test()
 {
-    //TODO
-
+    vector<int> v1 { 0, 0, 30, 20, 0, 0, 0, 0, 10, 0 };
+    int equal_to{ 0 }, n{ 2 };
+    std::cout << v1.size() << "\n";
+    vector<int> v2;
+    v2 = my_not_equal_to(v1, equal_to);
+    std::cout << "\n====v2====\n";
+    for (vector<int>::iterator it = v2.begin(); it < v2.end();it++)
+    {
+        std::cout << *it<< "\n";
+    }
 }
+
 int main()
 {
     Vector_Test();
